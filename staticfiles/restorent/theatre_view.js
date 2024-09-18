0b35ef13cb87fbd37408ca3d09c9e6d07c5b18eb
guru-sevak-singh <116969988+guru-sevak-singh@users.noreply.github.com>
2024-09-18 17:06:53 +0530

    Create theatre_view.js

---
+++ staticfiles/restorent/theatre_view.js
@@ -0,0 +1,99 @@
+// menue url
+const menue_url = '/show_menue/'
+
+// creating web socket url
+const websoclet_url = `wss://${window.location.host}/ws/dashboard_data/`
+
+// creating socket
+const socket = new WebSocket(websoclet_url)
+
+// Function which show any data come from socekt
+socket.onmessage = (e) => {
+    const data = JSON.parse(e.data);
+    UpdateNewData(data.dashboard_data)
+    playAudio();
+    console.log(data);
+}
+
+// function run on close of socket
+socket.onclose = (e) => {
+    console.error('socket is disconected');
+}
+
+// function to send data to socket
+function ChangeStatus(button) {
+    let status = button.innerText
+    if (status == 'Free'){
+        status = 'free';
+    }
+    const data = {
+        'pk': SelectedTableId,
+        'status': status,
+        'action': 'update_table_status'
+    }
+
+    // going data to socket
+    socket.send(JSON.stringify(data))
+}
+
+var SelectedTableId = ""
+
+// having a Selected Id to send data to socket
+function ChangeUrl(pk, div) {
+    SelectedTableId = pk;
+    let btn = document.getElementById('table-status')
+    let status = ""
+    if (div.getAttribute('class') == 'seat'){
+        status = "Bussy"
+        btn.setAttribute('class', 'btn btn-danger')
+
+    }
+    else{
+        status = "free"
+        btn.setAttribute('class', 'btn btn-success')
+    }
+    btn.innerText = status;
+    document.getElementById('url-button').setAttribute('href', `${menue_url}${pk}`)
+    let table_name = div.innerText;
+    document.getElementById('table-name').innerText = table_name;
+    $("#changeTableStatus").modal('show');
+}
+
+function playAudio() {
+    const audio = new Audio('/static/sound/notification.wav');
+    audio.play().catch(error => {
+        console.error('Error in Playing audio', error)
+    })
+}
+
+function UpdateNewData(data) {
+    let table_pk = data.pk;
+
+    let table = document.getElementById(`table-id-${table_pk}`);
+    if (table == null) {
+        // not to do anything
+        console.log('pass')
+    }
+
+    if (data.table_vacent_status == true) {
+        table.setAttribute('class', 'seat orderreceived');
+
+        if (data.payment_panding == false) {
+            table.setAttribute('class', 'seat paymentreceived')
+        }
+    }
+    else {
+        table.setAttribute('class', 'seat');
+    }
+
+    try{
+        let alert_message = data.alert_message
+        if (alert_message) {
+            alert(alert_message);
+        }
+    }
+    catch(error){
+        console.log(error)
+    }
+
+}
